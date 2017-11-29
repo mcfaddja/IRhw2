2a9639c2df664dc29067b62a8329ede8f1aad052
mcfaddja <mcfaddja@users.noreply.github.com>
2017-11-28 21:24:10 -0800

    finished M matrix and Initial r vector

--- IR-hw2.py
+++ IR-hw2.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 
+# constants
+beta = 0.85
+filename = "input.txt"
+
 # import file
-df = np.genfromtxt("input.txt", dtype=float)
-print(df)
+df = np.genfromtxt(filename, dtype=float)
+
 # get required size of matrix
 nDim0 = np.amax(df, axis=0)
-nDim = np.amax(nDim0[1:2])
+nDim = np.amax(nDim0[0:2])
 
 # create initial matrix
 Amat = np.zeros((nDim, nDim))
@@ -19,4 +23,36 @@ for row in df:
 	
 	Amat[i,j]=k
 	
-print(Amat)
+#print(Amat)
+
+# normalize by column
+colSums = Amat.sum(axis=0)
+#print(colSums)
+Amat = np.transpose(Amat)
+Mmat = np.zeros((nDim,nDim))
+for i, (row, colSums) in enumerate(zip(Amat, colSums)):
+	if colSums != 0:
+		Mmat[i,:] = row / colSums
+	else:
+		Mmat[i,:] = row
+
+# reverse the transpose		
+Mmat = np.transpose(Mmat)
+
+print("Normalized adjacency matrix")
+print(Mmat)
+print("")
+
+# create initial r vector
+r0 = np.ones((nDim,1))
+r0 = r0 * 1/nDim
+
+print("The original page rank vector")
+print(r0)
+print("")
+
+# iterate rank vector
+testCond = False
+while testCond == False:
+	r1 = 
+		
